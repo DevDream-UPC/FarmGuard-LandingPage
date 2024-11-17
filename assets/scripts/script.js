c124e0df1e35fc60adf33817c52c09f10f340f91
Software  Engineer Student | English | Spanish <102981505+Brays83@users.noreply.github.com>
2024-11-16 21:54:45 -0500

    feat[main]: added about us section

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,3 @@
 document.getElementById("action-button").addEventListener("click", function() {
-    window.location.href = "https://zingy-florentine-611560.netlify.app/";
+    window.location.href = "https://farmguard-frontend-915898677282.us-central1.run.app/auth/sign-in";
 });
